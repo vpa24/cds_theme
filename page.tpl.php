c5112c2e6d91fb0cf4193275cec25f728d23959b
vpa24 <vuphuonganh020497@gmail.com>
2019-09-13 12:55:31 +0700

    fix page file

--- page.tpl.php
+++ page.tpl.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700&amp;lang=en" />
 
 <head>
   <?php print $head ?>
   <title><?php print $head_title ?></title>
   <?php print $styles ?>
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700&amp;lang=en" />
   <?php print $scripts ?>
   <!--[if lt IE 7]>
       <?php print phptemplate_get_ie_styles(); ?>
@@ -46,62 +46,52 @@
             </div>
           </div>
         </div>
-      </div> <!-- /header -->
-      <di class="container-fluid">
-        <?php
-        if ($header) {
-          print $header;
-        }
-        ?>
+      </div>
 
-        <div id="body" class="row">
-          <?php if ($left) : ?>
-            <div id="sidebar-left" class="sidebar col-2 col-md-2 m-4">
-              <?php if ($search_box) : ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
-              <?php print $left ?>
-            </div>
-          <?php endif; ?>
+      <?php if ($header) : print $header;
+      endif; ?>
 
-          <div id="center" class="col">
+      <div id="body" class="row">
+        <?php if ($left) : ?>
+          <div id="sidebar-left" class="sidebar col-2 col-md-2 m-4">
+            <?php if ($search_box) : ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
+            <?php print $left ?>
+          </div>
+        <?php endif; ?>
 
-            <?php //print $breadcrumb;
-            ?>
-            <?php if ($mission) : print '<div id="mission">' . $mission . '</div>';
-            endif; ?>
-            <?php if ($tabs) : print '<div id="tabs-wrapper" class="clear-block">';
-            endif; ?>
-            <?php if ($title) : print '<h2' . ($tabs ? ' class="with-tabs"' : '') . '>' . $title . '</h2>';
-            endif; ?>
-            <?php if ($tabs) : print '<ul class="tabs primary">' . $tabs . '</ul></div>';
-            endif; ?>
-            <?php if ($tabs2) : print '<ul class="tabs secondary">' . $tabs2 . '</ul>';
-            endif; ?>
-            <?php if ($show_messages && $messages) : print $messages;
-            endif; ?>
-            <?php print $help; ?>
-            <div class=" clear-block">
-              <?php print $content ?>
-            </div>
-          </div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+        <div id="center" class="col">
+          <?php //print $breadcrumb;
+          ?>
+          <?php if ($mission) : print '<div id="mission">' . $mission . '</div>';
+          endif; ?>
+          <?php if ($tabs) : print '<div id="tabs-wrapper" class="clear-block">';
+          endif; ?>
+          <?php if ($title) : print '<h2' . ($tabs ? ' class="with-tabs"' : '') . '>' . $title . '</h2>';
+          endif; ?>
+          <?php if ($tabs) : print '<ul class="tabs primary">' . $tabs . '</ul></div>';
+          endif; ?>
+          <?php if ($tabs2) : print '<ul class="tabs secondary">' . $tabs2 . '</ul>';
+          endif; ?>
+          <?php if ($show_messages && $messages) : print $messages;
+          endif; ?>
+          <?php print $help; ?>
+          <?php print $content ?>
+        </div>
 
-          <?php if ($right) : ?>
-            <div id="sidebar-right" class="col-2 m-4">
-              <?php if (!$left && $search_box) : ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
-              <?php print $right ?>
-            </div>
-          <?php endif; ?>
+        <?php if ($right) : ?>
+          <div id="sidebar-right" class="col-2 m-4">
+            <?php if (!$left && $search_box) : ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
+            <?php print $right ?>
+          </div>
+        <?php endif; ?>
 
-        </div> <!-- /container -->
-
-
+      </div>
     </div>
-  </div>
   </div>
 
   <div class="row py-5 d-flex justify-content-center" id="footer">
     <?php print $footer_message . $footer ?>
   </div>
-  <!-- /layout -->
 </body>
 
 </html>
